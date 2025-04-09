f209ed65ce0d31d718e71ba94e3c62032d19eed7
nishujangra <ndjangra1027@gmail.com>
2025-04-09 11:06:43 +0530

    Blogs Updated

---
+++ src/data/blogs.ts
@@ -0,0 +1,38 @@
+export const blogs = [
+    {
+        title: "How to install KeyDB server in Debian 12(Bookworm) Linux using PPA Repositoies",
+        description:
+            "A detailed guide on installing KeyDB server in Debian 12 using PPA repositories.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/KeyDB-Setup",
+    },
+    {
+        title: "How to setup KeyDB servers with Multi-Master in Debian 12(Bookworm)",
+        description:
+            "Learn how to set up KeyDB servers with Multi-Master configuration in Debian 12.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/KeyDB-Multi-Master",
+    },
+    {
+        title: "Redis Cluster: What It Is and How to Set It Up on Devuan",
+        description:
+            "Learn how to set up a Redis Cluster on Devuan Linux, including configuration and management tips.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/Redis-Cluster",
+    },
+    {
+        title: "How to Install Redis on Devuan Linux (Daedalus 5.0)",
+        description:
+            "A step-by-step guide to installing Redis on Devuan Linux, covering prerequisites and installation commands.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/Redis",
+    },
+    {
+        title: "How to Add a User to the Sudoers in Devuan Linux (Daedalus 5.0)",
+        description:
+            "Learn how to add a user to the sudoers file in Devuan Linux for elevated privileges.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/Sudoers",
+    },
+    {
+        title: "How to Run C/C++ Code in Devuan Linux (Daedalus 5.0)",
+        description:
+            "A guide to compiling and running C/C++ code in Devuan Linux, including installation of necessary packages.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/C++",
+    }
+];
--- src/pages/Blogs/Blogs.tsx
+++ src/pages/Blogs/Blogs.tsx
@@ -1,3 +1,4 @@
+import { blogs } from "@/data/blogs";
 import { Link } from "react-router-dom";
 
 const Blogs = () => {
@@ -6,37 +7,25 @@ const Blogs = () => {
             <div className="flex flex-col justify-center items-center min-h-[80vh]">
                 <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg p-8">
                     <h1 className="text-4xl font-bold text-center mb-8">
-                        Link to My Devuan Wiki Blogs
+                        Blog Links
                     </h1>
                     <div className="mt-8">
                         <ul className="list-disc list-inside text-gray-400">
-                            <li className="mb-4">
-                                <Link
-                                    className="hover:underline"
-                                    to="https://bitbucket.org/tirveni/devuan/wiki/Redis-Cluster"
-                                >
-                                    Redis Cluster: What It Is and How to Set It
-                                    Up on Devuan
-                                </Link>
-                            </li>
-                            <li className="mb-4">
-                                <Link
-                                    className="hover:underline"
-                                    to="https://bitbucket.org/tirveni/devuan/wiki/Redis"
-                                >
-                                    How to Install Redis on Devuan Linux
-                                    (Daedalus 5.0)
-                                </Link>
-                            </li>
-                            <li className="mb-4">
-                                <Link
-                                    className="hover:underline"
-                                    to="https://bitbucket.org/tirveni/devuan/wiki/Sudoers"
-                                >
-                                    How to Add a User to the Sudoers in Devuan
-                                    Linux (Daedalus 5.0)
-                                </Link>
-                            </li>
+                            {blogs.map((blog, index) => (
+                                <li key={index} className="mb-4">
+                                    <Link
+                                        to={blog.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="text-blue-500 hover:underline"
+                                    >
+                                        {blog.title}
+                                    </Link>
+                                    <p className="text-gray-300">
+                                        {blog.description}
+                                    </p>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
