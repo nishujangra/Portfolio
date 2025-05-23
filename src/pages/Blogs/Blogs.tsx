c57c8f8bd394ab9ace76d3876984fe8be4058592
nishujangra <ndjangra1027@gmail.com>
2025-05-23 16:38:54 +0530

    Blogs design enhanced

--- src/pages/Blogs/Blogs.tsx
+++ src/pages/Blogs/Blogs.tsx
@@ -5,23 +5,23 @@ const Blogs = () => {
     return (
         <main className="p-6">
             <div className="flex flex-col justify-center items-center min-h-[80vh]">
-                <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg p-8">
-                    <h1 className="text-4xl font-bold text-center mb-8">
+                <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg p-8 border border-pink-200">
+                    <h1 className="text-5xl font-extrabold text-center mb-8 text-pink-600">
                         Blog Links
                     </h1>
                     <div className="mt-8">
-                        <ul className="list-disc list-inside text-gray-400">
+                        <ul className="list-disc list-inside text-gray-600">
                             {blogs.map((blog, index) => (
                                 <li key={index} className="mb-4">
                                     <Link
                                         to={blog.url}
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className="text-blue-500 hover:underline"
+                                        className="text-pink-500 hover:underline hover:text-pink-700 transition duration-300"
                                     >
                                         {blog.title}
                                     </Link>
-                                    <p className="text-gray-300">
+                                    <p className="text-gray-500 italic">
                                         {blog.description}
                                     </p>
                                 </li>
