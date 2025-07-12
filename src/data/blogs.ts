184b50f064ca013c5b40627fe3f35938538d43a8
nishujangra <ndjangra1027@gmail.com>
2025-07-12 12:21:19 +0530

    Blogs updated

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -1,21 +1,15 @@
 export const blogs = [
     {
-        title: "How to Set Up a Local DNS Server Using dnsmasq",
+        title: "Redis Cluster: What It Is and How to Set It Up on Devuan",
         description:
-            "Learn how to configure a local DNS server using dnsmasq for faster name resolution and local domain mapping.",
-        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Setting-Up-a-Local-DNS-Server-Using-dnsmasq",
+            "Learn how to set up a Redis Cluster on Devuan Linux, including configuration and management tips.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Redis-Cluster",
     },
     {
-        title: "How to Set Up a Virtual Gateway with NAT in VirtualBox",
+        title: "How to setup KeyDB servers with Multi-Master in Debian 12 (Bookworm)",
         description:
-            "Detailed instructions to configure a virtual NAT gateway in VirtualBox to enable internet access for VMs.",
-        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Setting-Up-a-Virtual-Gateway-with-NAT-in-VirtualBox",
-    },
-    {
-        title: "How to Install Apache CouchDB on Debian",
-        description:
-            "Step-by-step guide to installing Apache CouchDB on a Debian-based system, including required dependencies and service configuration.",
-        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Install-Apache-CouchDB-on-Debian",
+            "Learn how to set up KeyDB servers with Multi-Master configuration in Debian 12.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/KeyDB-Multi-Master",
     },
     {
         title: "Setting Up Samaritan Proxy to Load Balance Two KeyDB Servers",
@@ -24,22 +18,34 @@ export const blogs = [
         url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/SAM-Proxy",
     },
     {
-        title: "How to setup KeyDB servers with Multi-Master in Debian 12(Bookworm)",
+        title: "Ansible Lab Setup on Devuan",
         description:
-            "Learn how to set up KeyDB servers with Multi-Master configuration in Debian 12.",
-        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/KeyDB-Multi-Master",
+            "Guide to setting up a basic Ansible lab environment on Devuan for configuration management and automation practice.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Ansible-Lab",
     },
     {
-        title: "How to install KeyDB server in Debian 12(Bookworm) Linux using PPA Repositories",
+        title: "How to Set Up a Virtual Gateway with NAT in VirtualBox",
+        description:
+            "Detailed instructions to configure a virtual NAT gateway in VirtualBox to enable internet access for VMs.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Setting-Up-a-Virtual-Gateway-with-NAT-in-VirtualBox",
+    },
+    {
+        title: "How to Set Up a Local DNS Server Using dnsmasq",
+        description:
+            "Learn how to configure a local DNS server using dnsmasq for faster name resolution and local domain mapping.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Setting-Up-a-Local-DNS-Server-Using-dnsmasq",
+    },
+    {
+        title: "How to Install Apache CouchDB on Debian",
+        description:
+            "Step-by-step guide to installing Apache CouchDB on a Debian-based system, including required dependencies and service configuration.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Install-Apache-CouchDB-on-Debian",
+    },
+    {
+        title: "How to install KeyDB server in Debian 12 (Bookworm) Linux using PPA Repositories",
         description:
             "A detailed guide on installing KeyDB server in Debian 12 using PPA repositories.",
         url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/KeyDB-Setup",
-    },
-    {
-        title: "Redis Cluster: What It Is and How to Set It Up on Devuan",
-        description:
-            "Learn how to set up a Redis Cluster on Devuan Linux, including configuration and management tips.",
-        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Redis-Cluster",
     },
     {
         title: "How to Install Redis on Devuan Linux (Daedalus 5.0)",
@@ -48,15 +54,27 @@ export const blogs = [
         url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Redis-Installation",
     },
     {
-        title: "How to Run C/C++ Code in Devuan Linux (Daedalus 5.0)",
+        title: "Canine Setup: Lightweight Container Orchestration with Docker Compose",
         description:
-            "A guide to compiling and running C/C++ code in Devuan Linux, including installation of necessary packages.",
-        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/C++",
+            "Learn how to install and run Canine, a lightweight Docker-based container orchestration tool, with practical usage tips.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Canine-Setup",
+    },
+    {
+        title: "Reading Root-Protected Files with Sudo (No Password Prompt)",
+        description:
+            "Learn how to read files that require root access using sudo without being prompted for a password. A neat trick for sysadmins and developers.",
+        url: "https://blog.devops.dev/reading-root-protected-files-with-sudo-no-password-prompt-f70a9bdd8ca7",
     },
     {
         title: "How to Add a User to the Sudoers in Devuan Linux (Daedalus 5.0)",
         description:
             "Learn how to add a user to the sudoers file in Devuan Linux for elevated privileges.",
         url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/Sudoers",
+    },
+    {
+        title: "How to Run C/C++ Code in Devuan Linux (Daedalus 5.0)",
+        description:
+            "A guide to compiling and running C/C++ code in Devuan Linux, including installation of necessary packages.",
+        url: "https://bitbucket.org/tirveni/devuan/wiki/nishant/C++",
     }
 ];
